ebb09b05781e9f815991bcd5edee6ffcbce9ce3e
Inokcode <regilearn@gmail.com>
2021-07-21 14:24:28 +0530

    Project done

--- pages/inoks/index.js
+++ pages/inoks/index.js
@@ -1,7 +1,26 @@
-const Inoks = () => {
+import styles from '../../styles/inoks.module.css';
+import Link from 'next/link';
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  return {
+    props: {
+      users: data,
+    },
+  };
+};
+
+const Inoks = ({ users }) => {
   return (
     <div>
       <h1>All Inoks</h1>
+      {users.map((user) => (
+        <Link href={'/inoks/' + user.id} key={user.id}>
+          <a className={styles.single}>
+            <h3>{user.name}</h3>
+          </a>
+        </Link>
+      ))}
     </div>
   );
 };
